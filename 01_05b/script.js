f463b0f22ffce7f6f045f77c5e9fb6180c041b78
Tasyahan <130179834+tasyahan@users.noreply.github.com>
2023-09-30 22:35:16 +0000

    feat : license (spread syntax)

--- 01_05b/script.js
+++ 01_05b/script.js
@@ -9,6 +9,18 @@
 import data from "./data.js";
 import Cardlist from "./components/Cardlist.js";
 
+const license = {
+  license: "Unsplash License",
+  lincenseURL: "https://unsplash.com/license",
+};
+
+
+const newData =data.map((imgData)=>{
+  const newImgData ={...imgData, ...license}
+  return newImgData
+})
+
+
 const mainContent = document.querySelector(".main-content");
 
-mainContent.innerHTML = Cardlist(data);
+mainContent.innerHTML = Cardlist(newData);
